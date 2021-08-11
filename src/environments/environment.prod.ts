02018ef18dd67742a4ef06f36dc686bfa9c8831c
AdamLiu-kwdr <jtljtliu@outlook.com>
2021-08-11 16:01:32 +0800

    adjustments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  //Always use Server's IP address. Do not use localhost (Don't worry, local machine will reslove automatically.)
+  OrderManApiURL:'(connecction address)',
+};
